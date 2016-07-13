c0bf0c00c8057707394819a292d9dc5cf4489b4e
mattdell <mattdell29@gmail.com>
2016-07-13 21:25:48 +0100

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+	return (
+		<h2>Hello {props.name}</h2>
+	);
+};
+
+Hello.propTypes = {
+	name: PropTypes.string
+};
+
+ReactDOM.render(<Hello name='London'/>,
+	document.getElementById('greeting')
+);
